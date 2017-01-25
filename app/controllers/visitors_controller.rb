02d353daa052be2290a6bb413822dc31654b4474
Gagan <gagan.remba@gmail.com>
2017-01-25 18:35:53 +0530

    Initial commit

---
+++ app/controllers/visitors_controller.rb
@@ -0,0 +1,11 @@
+class VisitorsController < ApplicationController
+
+	# 
+	def index
+		if params[:q]
+			page = params[:page] || 1
+			@results = GoogleCustomSearchApi.search(params[:q],page: page)
+		end
+	end
+end
+
